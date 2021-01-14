d2705af4c0a771bb9c955fe4ef6c4deb64aff937
Brian Jenkins <studiojenkins@yahoo.com>
2021-01-14 07:50:13 -0500

    fresh commit for phase 2

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 
+// timer
+$(document).ready(function(){
+  var time=$(".time").text();
+  var interval=setInterval(function(){
+    var count = time.split(" : ");
+    var min = count [0];
+    var sec = count [1];
+    var counter = $(".timer").find("span");
+    counter.eq(0):text(min);
+    counter.eq(1):text(sec);
+  }
+  if (sec === 0){
+    if(min!= 0){
+      counter.eq(1).text(min--);
+      sec=59;
+    }
+  }
+    else if (min===0 && sec===0){
+      counter.eq(0).text(min);
+      counter.eq(1).text(sec);
+      time=counter.eq(0).text() + counter.eq(1).text();
+      clearInterval(interval);
+      alert("Time's Up!");
+    }
+  },1000);
+  
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$( "div.demo-container" ).html();
 
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback');
