e62947e96805c83d971a3dd3c2522779eed64380
Brian Jenkins <studiojenkins@yahoo.com>
2021-01-14 19:37:39 -0500

    complete revise

--- app.js
+++ app.js
@@ -1,60 +1,107 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-// timer
-$(document).ready(function(){
-  var time=$(".time").text();
-  var interval=setInterval(function(){
-    var count = time.split(" : ");
-    var min = count [0];
-    var sec = count [1];
-    var counter = $(".timer").find("span");
-    counter.eq(0):text(min);
-    counter.eq(1):text(sec);
-  }
-  if (sec === 0){
-    if(min!= 0){
-      counter.eq(1).text(min--);
-      sec=59;
-    }
-  }
-    else if (min===0 && sec===0){
-      counter.eq(0).text(min);
-      counter.eq(1).text(sec);
-      time=counter.eq(0).text() + counter.eq(1).text();
-      clearInterval(interval);
-      alert("Time's Up!");
-    }
-  },1000);
-  
-});
+let shuffledQuestions, currentQuestionIndex
 
-$( "div.demo-container" ).html();
-
-const form = document.querySelector('.signup-form');
-const feedback = document.querySelector('.feedback');
-const usernamePattern = /^[a-zA-Z]{6,12}$/;
-
-// validation
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const username =  form.username.value;
-
-  if(usernamePattern.test(username)){
-    feedback.textContent = 'that username is valid!'
-  } else {
-    feedback.textContent = 'username must contain only letters & be between 6 & 12 characters';
-  }
-});
-
-// live feedback
-form.username.addEventListener('keyup', e => {
-  // console.log(e.target.value, form.username.value);
-  if(usernamePattern.test(e.target.value)){
-    //console.log('passed');
-    form.username.setAttribute('class', 'success');
-  } else {
-    //console.log('failed');
-    form.username.setAttribute('class', 'error');
-  }
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+
+const questions = [
+    {
+    question: 'Who is the best golfer?',
+    answers: [
+    { text: 'Arnold Palmer', correct: true },
+    { text: 'Tiger Woods', correct: false },
+    { text: 'Brian Jenkins', correct: false },
+    { text: 'Brad Faxon', correct: false },
+    ]
+    },
+    {
+    question: 'Where is Pebble Beach?',
+    answers: [
+    { text: 'Florida', correct: false },
+    { text: 'Wisconsin', correct: false },
+    { text: 'California', correct: true },
+    { text: 'Texas', correct: false },
+    ]},
+
+    {question: 'When is The Masters?',
+    answers: [
+    { text: 'April', correct: true },
+    { text: 'June', correct: false },
+    { text: 'December', correct: false },
+    { text: 'May', correct: false }
+    ]
+}
+]
