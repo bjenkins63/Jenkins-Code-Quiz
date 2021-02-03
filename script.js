4b0180d67dd50b1bea11448a60af2f3ca8656cf5
Brian Jenkins <studiojenkins@yahoo.com>
2021-02-03 11:21:48 -0500

    more revisions to layout, still timer bugs

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var score = document.querySelector(".score");
+
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 
-var scoreCounter = 0;
 var timer;
-var timerCount = "";
+var timerCount = 20;
 
 
 function startTimer() {
@@ -17,118 +16,374 @@ function startTimer() {
   }, 1000);
 }
 
-function startGame() {
-  isScore = false;
-  timerCount = 20;
-  start-button.disabled; true;
-  startTimer()
-}
+// function begin01() {
+//   timerCount = 20;
+//   startTimer()
+// }
 
-var quiz = {
 
-  data: [
-    {
-      q: "Which player has won the most major championships in his career? (18)",
-      o: ["Arnold Palmer", "Jack Nicklas", "Brad Faxon", "Tiger Woods"],
-      a: 1
-    },
-    {
-      q: "Which year was golf’s last appearance at the Olympic Games before returning to the line-up of sports in 2016?",
-      o: ["1962", "1976", "1890", "1904"],
-      a: 3
-    },
-    {
-      q: "How many times has Tiger Woods won The Masters?",
-      o: ["7", "12", "5", "2"],
-      a: 2
-    },
-    {
-      q: "The 11th, 12th and 13th holes at Augusta National are collectively known by what nickname?",
-      o: ["Amen Corner", "The Snake Pit", "Magnolia Lane", "Hell on Earth"],
-      a: 0
-    },
-    {
-      q: "How many clubs are golfers allowed to carry in their bag during a professional competition?",
-      o: ["10", "17", "11", "14"],
-      a: 3
-    }
-  ],
+var q = ["If you score a 3 on a par 4, what did you make?<br /><br />", "How many holes on a regulation golf course?<br /><br />", "Who plays first?<br /><br />", "If you score a 6 on a par 5, what did you make?<br /><br />", "How many clubs can you have in your bag?<br /><br />", "Who holds the record for most Major tournament victories?<br /><br />", "where is the Masters Tournament held?<br /><br />", "What does it mean when you score an eagle?<br /><br />", "When can you mark your ball and pick it up?<br /><br />", "What is a mulligan?<br /><br />"];
+        var a1 = ["<button class=buttons002 onclick=q1c()>Birdie</button>",
+                  "<button class=buttons002 onclick=q2c()>18</button>",
+                  "<button class=buttons002 onclick=q3i()>Flip a tee</button>",
+                  "<button class=buttons002 onclick=q4i()>Birdie</button>",
+                  "<button class=buttons002 onclick=q5i()>12</button>",
+                  "<button class=buttons002 onclick=q6i()>Tiger Woods</button>",
+                  "<button class=buttons002 onclick=q7i()>Los Angeles<button>",
+                  "<button class=buttons002 onclick=q8i()>You won</button>",
+                  "<button class=buttons002 onclick=q9i()>In a penalty area</button>",
+                  "<button class=buttons002 onclick=q10i()>A stupid mistake</button>"];
 
-  // (A2) HTML ELEMENTS
-  hWrap: null, // HTML quiz container
-  hQn: null, // HTML question wrapper
-  hAns: null, // HTML answers wrapper
+        var a2 = ["<button class=buttons002 onclick=q1i()>Par</button>",
+                  "<button class=buttons002 onclick=q2i()>9</button>",
+                  "<button class=buttons002 onclick=q3c()>Furthest from the hole</button>",
+                  "<button class=buttons002 onclick=q4c()>Bogey</button>",
+                  "<button class=buttons002 onclick=q5c()>14</button>",
+                  "<button class=buttons002 onclick=q6c()>Jack Nicklas</button>",
+                  "<button class=buttons002 onclick=q7i()>Boston</button>",
+                  "<button class=buttons002 onclick=q8i()>Penalty stroke</button>",
+                  "<button class=buttons002 onclick=q9i()>In the rough</button>",
+                  "<button class=buttons002 onclick=q10i()>A hole in one</button>"];
 
-  // (A3) GAME FLAGS
-  now: 0, // current question
-  score: 0, // current score
+        var a3 = ["<button class=buttons002 onclick=q1i()>Bogey</button>",
+                  "<button class=buttons002 onclick=q2i()>36</button>",
+                  "<button class=buttons002 onclick=q3i()>Closest to the hole</button>",
+                  "<button class=buttons002 onclick=q4i()>Double Bogey</button>",
+                  "<button class=buttons002 onclick=q5i()>9</button>",
+                  "<button class=buttons002 onclick=q6i()>Arnold Palmer</button>",
+                  "<button class=buttons002 onclick=q7c()>Augusta</button>",
+                  "<button class=buttons002 onclick=q8c()>You scored 2 under par on a hole</button>",
+                  "<button class=buttons002 onclick=q9i()>In a pond</button>",
+                  "<button class=buttons002 onclick=q10i()>A bad golfer</button>"];
 
-  // (B) INIT QUIZ HTML
-  init: function () {
-    // (B1) WRAPPER
-    quiz.hWrap = document.getElementById("quizWrap");
+        var a4 = ["<button class=buttons002 onclick=q1i()>Eagle</button>",
+                  "<button class=buttons002 onclick=q2i()>12</button>",
+                  "<button class=buttons002 onclick=q3i()>Better golfer</button>",
+                  "<button class=buttons002 onclick=q4i()>Par</button>",
+                  "<button class=buttons002 onclick=q5i()>10</button>",
+                  "<button class=buttons002 onclick=q6i()>Bill Murray</button>",
+                  "<button class=buttons002 onclick=q7i()>Ireland</button>",
+                  "<button class=buttons002 onclick=q8i()>You parred the hole</button>",
+                  "<button class=buttons002 onclick=q9c()>On the green</button>",
+                  "<button class=buttons002 onclick=q10c()>A free replay</button>"];
 
-    // (B2) QUESTIONS SECTION
-    quiz.hQn = document.createElement("div");
-    quiz.hQn.id = "quizQn";
-    quiz.hWrap.appendChild(quiz.hQn);
 
-    // (B3) ANSWERS SECTION
-    quiz.hAns = document.createElement("div");
-    quiz.hAns.id = "quizAns";
-    quiz.hWrap.appendChild(quiz.hAns);
+        var n = 0;
+        n++;
+        var s = 0;
+        s++;
 
-    // (B4) GO!
-    quiz.draw();
-  },
+        function begin001() {
+            disappear001.innerHTML = "";
+            message001.innerHTML = "";
+            question001.innerHTML = q[0];
+            option001.innerHTML = a1[0];
+            option002.innerHTML = a2[0];
+            option003.innerHTML = a3[0];
+            option004.innerHTML = a4[0];
+            number001.innerHTML = n++;
+        }
 
-  // (C) DRAW QUESTION
-  draw: function () {
-    // (C1) QUESTION
-    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+        function q1c() {
+            answer001.innerHTML = "<div id=green001>" + "Correct!" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new002()>Next</button>";
+            score001.innerHTML = s++;
+        }
 
-    // (C2) OPTIONS
-    quiz.hAns.innerHTML = "";
-    for (let i in quiz.data[quiz.now].o) {
-      let radio = document.createElement("input");
-      radio.type = "radio";
-      radio.name = "quiz";
-      radio.id = "quizo" + i;
-      quiz.hAns.appendChild(radio);
-      let label = document.createElement("label");
-      label.innerHTML = quiz.data[quiz.now].o[i];
-      label.setAttribute("for", "quizo" + i);
-      label.dataset.idx = i;
-      label.addEventListener("click", quiz.select);
-      quiz.hAns.appendChild(label);
-    }
-  },
+        function q1i() {
+            answer001.innerHTML = "<div id=red001>" + "No" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new002()>Next</button>";
+        }
 
-  select: function () {
-    // (D1) DETACH ALL ONCLICK
-    let all = quiz.hAns.getElementsByTagName("label");
-    for (let label of all) {
-      label.removeEventListener("click", quiz.select);
-    }
+        function new002() {
+            question001.innerHTML = q[1];
+            option001.innerHTML = a1[1];
+            option002.innerHTML = a2[1];
+            option003.innerHTML = a3[1];
+            option004.innerHTML = a4[1];
+            next001.innerHTML = "";
+            answer001.innerHTML = "";
+            number001.innerHTML = n++;
+        }
 
-    // (D2) CHECK IF CORRECT
-    let correct = this.dataset.idx == quiz.data[quiz.now].a;
-    if (correct) {
-      quiz.score++;
-      this.classList.add("correct");
-    } else {
-      this.classList.add("wrong");
-    }
+        function q2c() {
+            answer001.innerHTML = "<div id=green001>" + "Yes!" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new003()>Next</button>";
+            score001.innerHTML = s++;
+        }
 
-    // (D3) NEXT QUESTION OR END GAME
-    quiz.now++;
-    setTimeout(function () {
-      if (quiz.now < quiz.data.length) { quiz.draw(); }
-      else {
-        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
-        quiz.hAns.innerHTML = "";
-      }
-    }, 1000);
-  }
-};
-window.addEventListener("load", quiz.init);
+        function q2i() {
+            answer001.innerHTML = "<div id=red001>" + "Um, no" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new003()>Next</button>";
+        }
+
+        function new003() {
+            question001.innerHTML = q[2];
+            option001.innerHTML = a1[2];
+            option002.innerHTML = a2[2];
+            option003.innerHTML = a3[2];
+            option004.innerHTML = a4[2];
+            next001.innerHTML = "";
+            answer001.innerHTML = "";
+            number001.innerHTML = n++;
+        }
+
+        function q3c() {
+            answer001.innerHTML = "<div id=green001>" + "Nice shot!" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new004()>Next</button>";
+            score001.innerHTML = s++;
+        }
+
+        function q3i() {
+            answer001.innerHTML = "<div id=red001>" + "Are you kidding?" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new004()>Next</button>";
+        }
+
+        function new004() {
+            question001.innerHTML = q[3];
+            option001.innerHTML = a1[3];
+            option002.innerHTML = a2[3];
+            option003.innerHTML = a3[3];
+            option004.innerHTML = a4[3];
+            next001.innerHTML = "";
+            answer001.innerHTML = "";
+            number001.innerHTML = n++;
+        }
+
+        function q4c() {
+            answer001.innerHTML = "<div id=green001>" + "You're a pro!" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new005()>Next</button>";
+            score001.innerHTML = s++;
+        }
+
+        function q4i() {
+            answer001.innerHTML = "<div id=red001>" + "Stay away from golf" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new005()>Next</button>";
+        }
+
+        function new005() {
+            question001.innerHTML = q[4];
+            option001.innerHTML = a1[4];
+            option002.innerHTML = a2[4];
+            option003.innerHTML = a3[4];
+            option004.innerHTML = a4[4];
+            next001.innerHTML = "";
+            answer001.innerHTML = "";
+            number001.innerHTML = n++;
+        }
+
+        function q5c() {
+            answer001.innerHTML = "<div id=green001>" + "Correct!" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new006()>Next</button>";
+            score001.innerHTML = s++;
+        }
+
+        function q5i() {
+            answer001.innerHTML = "<div id=red001>" + "No" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new006()>Next</button>";
+        }
+
+        function new006() {
+            question001.innerHTML = q[5];
+            option001.innerHTML = a1[5];
+            option002.innerHTML = a2[5];
+            option003.innerHTML = a3[5];
+            option004.innerHTML = a4[5];
+            next001.innerHTML = "";
+            answer001.innerHTML = "";
+            number001.innerHTML = n++;
+        }
+
+        function q6c() {
+            answer001.innerHTML = "<div id=green001>" + "YOU ROCK" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new007()>Next</button>";
+            score001.innerHTML = s++;
+        }
+
+        function q6i() {
+            answer001.innerHTML = "<div id=red001>" + "WOW, no" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new007()>Next</button>";
+        }
+
+        function new007() {
+            question001.innerHTML = q[6];
+            option001.innerHTML = a1[6];
+            option002.innerHTML = a2[6];
+            option003.innerHTML = a3[6];
+            option004.innerHTML = a4[6];
+            next001.innerHTML = "";
+            answer001.innerHTML = "";
+            number001.innerHTML = n++;
+        }
+
+        function q7c() {
+            answer001.innerHTML = "<div id=green001>" + "Correct" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new008()>Next</button>";
+            score001.innerHTML = s++;
+        }
+
+        function q7i() {
+            answer001.innerHTML = "<div id=red001>" + "No" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new008()>Next</button>";
+        }
+
+        function new008() {
+            question001.innerHTML = q[7];
+            option001.innerHTML = a1[7];
+            option002.innerHTML = a2[7];
+            option003.innerHTML = a3[7];
+            option004.innerHTML = a4[7];
+            next001.innerHTML = "";
+            answer001.innerHTML = "";
+            number001.innerHTML = n++;
+        }
+
+        function q8c() {
+            answer001.innerHTML = "<div id=green001>" + "Correct" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new009()>Next</button>";
+            score001.innerHTML = s++;
+        }
+
+        function q8i() {
+            answer001.innerHTML = "<div id=red001>" + "WRONG" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new009()>Next</button>";
+        }
+
+        function new009() {
+            question001.innerHTML = q[8];
+            option001.innerHTML = a1[8];
+            option002.innerHTML = a2[8];
+            option003.innerHTML = a3[8];
+            option004.innerHTML = a4[8];
+            next001.innerHTML = "";
+            answer001.innerHTML = "";
+            number001.innerHTML = n++;
+        }
+
+        function q9c() {
+            answer001.innerHTML = "<div id=green001>" + "you got it" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new010()>Next</button>";
+            score001.innerHTML = s++;
+        }
+
+        function q9i() {
+            answer001.innerHTML = "<div id=red001>" + "Duh" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=new010()>Next</button>";
+        }
+
+        function new010() {
+            question001.innerHTML = q[9];
+            option001.innerHTML = a1[9];
+            option002.innerHTML = a2[9];
+            option003.innerHTML = a3[9];
+            option004.innerHTML = a4[9];
+            next001.innerHTML = "";
+            answer001.innerHTML = "";
+            number001.innerHTML = n++;
+        }
+
+        function q10c() {
+            answer001.innerHTML = "<div id=green001>" + "Correct" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=end001()>End of Quiz</button>";
+            score001.innerHTML = s++;
+        }
+
+        function q10i() {
+            answer001.innerHTML = "<div id=red001>" + "Go back to school" + "</div>";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<button class=buttons001 onclick=end001()>End of Quiz</button>";
+        }
+
+        function end001() {
+            message001.innerHTML = "End of Quiz.";
+            question001.innerHTML = "";
+            option001.innerHTML = "";
+            option002.innerHTML = "";
+            option003.innerHTML = "";
+            option004.innerHTML = "";
+            next001.innerHTML = "<div id=text001>" + "<button class=buttons001 onclick=repeat001()>Repeat</button>" + "</div>";
+            answer001.innerHTML = "";
+        }
+
+        function repeat001() {
+            location.reload();
+        }
