089305178de2f2c32dc8ed45e59d39183aaa19c7
Brian Jenkins <studiojenkins@yahoo.com>
2021-02-02 22:12:59 -0500

    updated styles trouble shooting timer and score

--- script.js
+++ script.js
@@ -1,208 +1,134 @@
-  var score = document.querySelector(".win");
-  var lose = document.querySelector(".lose");
-  var timerElement = document.querySelector(".timer-count");
-  var startButton = document.querySelector(".start-button");
+var score = document.querySelector(".score");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
 
-  var scoreCounter = 0;
-  var timer;
-  var timerCount;
-
-  var questions = [
-    {
-      q: "what is this?",
-      a: [
-        "right answer", value = "1",
-        "nope", value = "0",
-        "nope", value = "0",
-        "nope", value = "0",
-      ]
-    },
-    {
-      q: "Question 2",
-      a: [
-        "right answer", value = "1",
-        "nope", value = "0",
-        "nope", value = "0",
-        "nope", value = "0",
-      ]
-    },
-    {
-      q: "Question 3",
-      a: [
-        "right answer", value = "1",
-        "nope", value = "0",
-        "nope", value = "0",
-        "nope", value = "0",
-      ]
-    },
-    {
-      q: "Question 4",
-      a: [
-        "right answer", value = "1",
-        "nope", value = "0",
-        "nope", value = "0",
-        "nope", value = "0",
-      ]
-    },
-    {
-      q: "Question 5",
-      a: [
-        "right answer", value = "1",
-        "nope", value = "0",
-        "nope", value = "0",
-        "nope", value = "0",
-      ]
-    },
-    {
-      q: "Question 6",
-      a: [
-        "right answer", value = "1",
-        "nope", value = "0",
-        "nope", value = "0",
-        "nope", value = "0",
-      ]
-    },
-
-  ];
-
-  var questions = [];
-  var ansers = ["variable", "array", "modulus", "object", "function", "string", "boolean"];
-
-  startButton.addEventListener("click", startGame);
-  // Calls init() so that it fires when page opened
-  init();
-  scoreCounter = 0;
-  setScore()
-  function setScore() {
-    win.textContent = scoreCounter;
-    localStorage.setItem("scoreCount", scoreCounter);
-  }
-
-  function startTimer() {
-    // Sets timer
-    timer = setInterval(function () {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        count++;
-      }
-      // Tests if time has run out
-      else (timerCount === 0)
-        // Clears interval
-        clearInterval(timer);
-        loseGame();
-      }
-    , 1000);
-  }
+var scoreCounter = 0;
+var timer;
+var timerCount = "";
 
 
-  // The startGame function is called when the start button is clicked
-  function startGame() {
-    timerCount = 20;
-    // Prevents start button from being clicked when round is in progress
-    startButton.disabled = false;
-    startTimer()
-  }
-
-  // These functions are used by init
-  function getWins() {
-    // Get stored value from client storage, if it exists
-    var storedWins = localStorage.getItem("winCount");
-    // If stored value doesn't exist, set counter to 0
-    if (storedWins === null) {
-      winCounter = 0;
-    } else {
-      // If a value is retrieved from client storage set the winCounter to that value
-      winCounter = storedWins;
+function startTimer() {
+  timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount === 0) {
+      clearInterval(timer);
     }
-    //Render win count to page
-    win.textContent = winCounter;
+  }, 1000);
+}
+
+function startGame() {
+  isScore = false;
+  timerCount = 20;
+  start-button.disabled; true;
+  startTimer()
+}
+
+var quiz = {
+
+  data: [
+    {
+      q: "Which player has won the most major championships in his career? (18)",
+      o: ["Arnold Palmer", "Jack Nicklas", "Brad Faxon", "Tiger Woods"],
+      a: 1
+    },
+    {
+      q: "Which year was golf’s last appearance at the Olympic Games before returning to the line-up of sports in 2016?",
+      o: ["1962", "1976", "1890", "1904"],
+      a: 3
+    },
+    {
+      q: "How many times has Tiger Woods won The Masters?",
+      o: ["7", "12", "5", "2"],
+      a: 2
+    },
+    {
+      q: "The 11th, 12th and 13th holes at Augusta National are collectively known by what nickname?",
+      o: ["Amen Corner", "The Snake Pit", "Magnolia Lane", "Hell on Earth"],
+      a: 0
+    },
+    {
+      q: "How many clubs are golfers allowed to carry in their bag during a professional competition?",
+      o: ["10", "17", "11", "14"],
+      a: 3
+    }
+  ],
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: function () {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: function () {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", quiz.select);
+      quiz.hAns.appendChild(label);
+    }
+  },
+
+  select: function () {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = this.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      this.classList.add("correct");
+    } else {
+      this.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(function () {
+      if (quiz.now < quiz.data.length) { quiz.draw(); }
+      else {
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
+      }
+    }, 1000);
   }
-
-
-  // function getlosses() {
-  //   var storedLosses = localStorage.getItem("loseCount");
-  //   if (storedLosses === null) {
-  //     loseCounter = 0;
-  //   } else {
-  //     loseCounter = storedLosses;
-  //   }
-  //   lose.textContent = loseCounter;
-  // }
-
-
-  // The winGame function is called when the win condition is met
-  // function winGame() {
-  //   wordBlank.textContent = "YOU WON!!! ";
-  //   winCounter++
-  //   startButton.disabled = false;
-  //   setWins()
-  // }
-
-  // The loseGame function is called when timer reaches 0
-  // function loseGame() {
-  //   wordBlank.textContent = "GAME OVER";
-  //   loseCounter++
-  //   startButton.disabled = false;
-  //   setLosses()
-  // }
-
-  // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-
-  // function checkWin() {
-  //   // If the word equals the blankLetters array when converted to string, set isWin to true
-  //   if (chosenWord === blanksLetters.join("")) {
-  //     // This value is used in the timer function to test if win condition is met
-  //     isWin = true;
-  //   }
-  // }
-
-  // // Tests if guessed letter is in word and renders it to the screen.
-  // function checkLetters(letter) {
-  //   var letterInWord = false;
-  //   for (var i = 0; i < numBlanks; i++) {
-  //     if (chosenWord[i] === letter) {
-  //       letterInWord = true;
-  //     }
-  //   }
-  //   if (letterInWord) {
-  //     for (var j = 0; j < numBlanks; j++) {
-  //       if (chosenWord[j] === letter) {
-  //         blanksLetters[j] = letter;
-  //       }
-  //     }
-  //     wordBlank.textContent = blanksLetters.join(" ");
-  //   }
-  // }
-
-  // // Attach event listener to document to listen for key event
-  // document.addEventListener("keydown", function(event) {
-  //   // If the count is zero, exit function
-  //   if (timerCount === 0) {
-  //     return;
-  //   }
-  //   // Convert all keys to lower case
-  //   var key = event.key.toLowerCase();
-  //   var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  //   // Test if key pushed is letter
-  //   if (alphabetNumericCharacters.includes(key)) {
-  //     var letterGuessed = event.key;
-  //     checkLetters(letterGuessed)
-  //     checkWin();
-  //   }
-  // });
-
-    // function startGame() {
-  //   timerCount = 10;
-  //   // Prevents start button from being clicked when round is in progress
-  //   startButton.disabled = true;
-  //   startTimer()
-  // }
-
-  // The init function is called when the page loads 
-  // function init() {
-  //   getWins();
-  // }
-
-
-
+};
+window.addEventListener("load", quiz.init);
